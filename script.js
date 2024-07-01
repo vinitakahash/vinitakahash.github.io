4d3eade3401254627e15611db429420496b080a3
Vinícius <169078039+vinitakahash@users.noreply.github.com>
2024-07-01 11:23:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 6000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 4){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
+
+
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
